7df2306f1499d46a9a5b7b431ac41f7dcb3a5d6b
Ilya-PC\Ilya <filippovilya90@gmail.com>
2017-06-11 11:29:50 +0300

    Add project of GameStore to repository

---
+++ GameStore/GameStore.WebUI/Controllers/GameController.cs
@@ -0,0 +1,25 @@
+﻿using GameStore.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GameStore.WebUI.Controllers
+{
+    public class GameController : Controller
+    {
+		IGameRepository repository;
+		public GameController(IGameRepository repo)
+		{
+			repository = repo;
+		}
+
+		public ViewResult List()
+		{
+			var game = repository.Games;
+
+			return View(game.First());
+		}
+    }
+}
