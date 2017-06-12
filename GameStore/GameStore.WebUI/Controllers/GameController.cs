dc146cee9cd95839d2dd9e026b1908f8c482d31e
Ilya-PC\Ilya <filippovilya90@gmail.com>
2017-06-12 12:28:32 +0300

    add test and param count of products in page

--- GameStore/GameStore.WebUI/Controllers/GameController.cs
+++ GameStore/GameStore.WebUI/Controllers/GameController.cs
@@ -10,15 +10,20 @@ namespace GameStore.WebUI.Controllers
     public class GameController : Controller
     {
 		IGameRepository repository;
+		public int pageSize = 4;
 
 		public GameController(IGameRepository repo)
 		{
 			repository = repo;
 		}
 
-		public ViewResult List()
+		public ViewResult List(int page=1)
 		{
-			return View(repository.Games);
+			return View(repository.Games
+				.OrderBy(game=>game.GameId)
+				.Skip((page-1)*pageSize)
+				.Take(pageSize)
+				);
 		}
     }
 }
