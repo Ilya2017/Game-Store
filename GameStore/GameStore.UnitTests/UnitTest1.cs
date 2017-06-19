5883900cd061977f7eaba6bd877bb2852f4d42be
Ilya-PC\Ilya <filippovilya90@gmail.com>
2017-06-19 19:37:33 +0300

    add navButtons for category

--- GameStore/GameStore.UnitTests/UnitTest1.cs
+++ GameStore/GameStore.UnitTests/UnitTest1.cs
@@ -144,5 +144,57 @@ namespace GameStore.UnitTests
 			Assert.AreEqual(results[1], "Симулятор");
 			Assert.AreEqual(results[2], "Шутер");
 		}
+
+		[TestMethod]
+		public void Indicates_Selected_Category()
+		{
+			// Организация - создание имитированного хранилища
+			Mock<IGameRepository> mock = new Mock<IGameRepository>();
+			mock.Setup(m => m.Games).Returns(new Game[] {
+				new Game { GameId = 1, Name = "Игра1", Category="Симулятор"},
+				new Game { GameId = 2, Name = "Игра2", Category="Шутер"}
+			});
+
+			// Организация - создание контроллера
+			NavController target = new NavController(mock.Object);
+
+			// Организация - определение выбранной категории
+			string categoryToSelect = "Шутер";
+
+			// Действие
+			string result = target.Menu(categoryToSelect).ViewBag.SelectedCategory;
+
+			// Утверждение
+			Assert.AreEqual(categoryToSelect, result);
+		}
+
+		[TestMethod]
+		public void Generate_Category_Specific_Game_Count()
+		{
+			/// Организация (arrange)
+			Mock<IGameRepository> mock = new Mock<IGameRepository>();
+			mock.Setup(m => m.Games).Returns(new List<Game>
+			{
+				new Game { GameId = 1, Name = "Игра1", Category="Cat1"},
+				new Game { GameId = 2, Name = "Игра2", Category="Cat2"},
+				new Game { GameId = 3, Name = "Игра3", Category="Cat1"},
+				new Game { GameId = 4, Name = "Игра4", Category="Cat2"},
+				new Game { GameId = 5, Name = "Игра5", Category="Cat3"}
+			});
+			GameController controller = new GameController(mock.Object);
+			controller.pageSize = 3;
+
+			// Действие - тестирование счетчиков товаров для различных категорий
+			int res1 = ((GamesListViewModel)controller.List("Cat1").Model).PagingInfo.TotalItems;
+			int res2 = ((GamesListViewModel)controller.List("Cat2").Model).PagingInfo.TotalItems;
+			int res3 = ((GamesListViewModel)controller.List("Cat3").Model).PagingInfo.TotalItems;
+			int resAll = ((GamesListViewModel)controller.List(null).Model).PagingInfo.TotalItems;
+
+			// Утверждение
+			Assert.AreEqual(res1, 2);
+			Assert.AreEqual(res2, 2);
+			Assert.AreEqual(res3, 1);
+			Assert.AreEqual(resAll, 5);
+		}
 	}
 }
